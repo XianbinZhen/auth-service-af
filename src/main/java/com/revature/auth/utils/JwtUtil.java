c67611cb4960ba18cd8ccc4847ff6cc7782b9067
Aristotle <a.debride.2016@gmail.com>
2021-03-31 11:19:15 -0400

    JWT util

--- src/main/java/com/revature/auth/utils/JwtUtil.java
+++ src/main/java/com/revature/auth/utils/JwtUtil.java
@@ -1,16 +1,24 @@
 package com.revature.auth.utils;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 // TODO JwtUtil methods need to be implemented.
 public class JwtUtil {
+
+    private static final Algorithm ALGORITHM = Algorithm.HMAC256(System.getenv("AF_SECRET"));
+
     public static String generate(String email, String role) {
 
-        return "";
+        return JWT.create()
+                .withClaim("email", email)
+                .withClaim("role", role)
+                .sign(ALGORITHM);
     }
 
     public static DecodedJWT isValidJWT(String jwt) {
 
-        return null;
+        return JWT.require(ALGORITHM).build().verify(jwt);
     }
 }
